a4bddc40394fc5ac30e9b063212e1d866b7aa129
shontzu alias yuuchin <shon.tzu@besquare.com.my>
2022-08-09 12:45:36 +0800

    cubit, service, and model for pokemon_list

---
+++ lib/models/pokemon_list_model.dart
@@ -0,0 +1,58 @@
+import 'package:meta/meta.dart';
+import 'dart:convert';
+
+class PokemonListModel {
+  PokemonListModel({
+    required this.count,
+    required this.next,
+    required this.previous,
+    required this.results,
+  });
+
+  final int count;
+  final String next;
+  final dynamic previous;
+  final List<Result> results;
+
+  factory PokemonListModel.fromJson(String str) => PokemonListModel.fromMap(json.decode(str));
+
+  String toJson() => json.encode(toMap());
+
+  factory PokemonListModel.fromMap(Map<String, dynamic> json) => PokemonListModel(
+    count: json["count"],
+    next: json["next"],
+    previous: json["previous"],
+    results: List<Result>.from(json["results"].map((x) => Result.fromMap(x))),
+  );
+
+  Map<String, dynamic> toMap() => {
+    "count": count,
+    "next": next,
+    "previous": previous,
+    "results": List<dynamic>.from(results.map((x) => x.toMap())),
+  };
+}
+
+class Result {
+  Result({
+    required this.name,
+    required this.url,
+  });
+
+  final String name;
+  final String url;
+
+  factory Result.fromJson(String str) => Result.fromMap(json.decode(str));
+
+  String toJson() => json.encode(toMap());
+
+  factory Result.fromMap(Map<String, dynamic> json) => Result(
+    name: json["name"],
+    url: json["url"],
+  );
+
+  Map<String, dynamic> toMap() => {
+    "name": name,
+    "url": url,
+  };
+}
