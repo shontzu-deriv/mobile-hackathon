696ed7f90b26fd31d7bb0e3c61245fefe9ea3de0
shontzu alias yuuchin <shon.tzu@besquare.com.my>
2022-08-09 13:55:30 +0800

    rendered gif

--- lib/pages/landing_page.dart
+++ lib/pages/landing_page.dart
@@ -1,7 +1,11 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import '../states/pokemon_cubit.dart';
 import '../states/pokemon_list_cubit.dart';
 import '../states/pokemon_list_states.dart';
+import '../states/pokemon_states.dart';
+import '../widgets/pokemon_information.dart';
 import 'home_page.dart';
 
 class LandingPage extends StatelessWidget {
@@ -24,16 +28,41 @@ class LandingPage extends StatelessWidget {
                   return const CircularProgressIndicator();
 
                 if (state is PokemonListLoaded) {
-                  return Column(
-                    children: [
-                      Text("fetch success"),
-                      Text("${state.pokemonListModel.results[4].name}")
-                    ],
-                  );
+                  return GridView.count(
+                    // Create grid with 2 columns (scrollDirection horizontal produces 2 rows)
+                    crossAxisCount: 2,
+                    // Generate 100 widgets that display their index in the List.
+                    children: List.generate(
+                        state.pokemonListModel.results.length, (i) {
+                      return Card(
+                        // color: Colors.orange[200],
+                        shape: RoundedRectangleBorder(
+                          side: BorderSide(color: Colors.grey, width: 0.5),
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        margin: EdgeInsets.all(10.0),
+                        child: Container(
+                          child: Column(
+                            children: <Widget>[
+                              ListTile(
+                                title: Text(
+                                    "${state.pokemonListModel.results[i].name}"),
+                              ),
+                              Image.network(
+                                "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/${i + 1}.gif",
+                                // height: 100,
+                                // width: 100,
+                              )
+                            ],
+                          ),
+                        ),
+                      );
+                    }),
+                  ); // Text("${state.pokemonListModel.results[4].name}")
                 }
 
                 return const Text(
-                    "some error occurred while fetching the data");
+                    "some error occurred while fetching the pokemons");
               })),
       floatingActionButton: ElevatedButton(
         onPressed: () {
@@ -44,7 +73,7 @@ class LandingPage extends StatelessWidget {
               ));
         },
         child: const Text(
-          "POKEMON WAR",
+          "BATTLE",
           style: TextStyle(fontSize: 20),
         ),
       ),
@@ -52,3 +81,71 @@ class LandingPage extends StatelessWidget {
     );
   }
 }
+
+// import 'package:flutter/material.dart';
+// import 'package:flutter_pokemon_app/pages/home_page.dart';
+//
+// class LandingPage extends StatefulWidget {
+//   const LandingPage({Key? key}) : super(key: key);
+//
+//   @override
+//   State<LandingPage> createState() => _LandingPageState();
+// }
+//
+// class _LandingPageState extends State<LandingPage> {
+//   late TextEditingController _pokeController;
+//
+//   @override
+//   void initState() {
+//     super.initState();
+//     _pokeController = TextEditingController();
+//   }
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(title: const Center(child: Text("Pokemon App"))),
+//       body: GridView.count(
+//         // Create grid with 2 columns (scrollDirection horizontal produces 2 rows)
+//         crossAxisCount: 2,
+//         // Generate 100 widgets that display their index in the List.
+//         children: List.generate(100, (index) {
+//           return Card(
+//             // color: Colors.orange[200],
+//             shape: RoundedRectangleBorder(
+//               side: BorderSide(color: Colors.grey, width: 0.5),
+//               borderRadius: BorderRadius.circular(10),
+//             ),
+//             margin: EdgeInsets.all(10.0),
+//             child: Container(
+//               child: Column(
+//                 children: <Widget>[
+//                   ListTile(
+//                     title: Text(
+//                       'Pokemon $index',
+//                       style: TextStyle(fontSize: 18),
+//                     ),
+//                   ),
+//                 ],
+//               ),
+//             ),
+//           );
+//         }),
+//       ),
+//       floatingActionButton: ElevatedButton(
+//         onPressed: () {
+//           Navigator.push(
+//               context,
+//               MaterialPageRoute(
+//                 builder: (context) => const Homepage(),
+//               ));
+//         },
+//         child: const Text(
+//           "POKEMON WAR",
+//           style: TextStyle(fontSize: 20),
+//         ),
+//       ),
+//       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+//     );
+//   }
+// }
