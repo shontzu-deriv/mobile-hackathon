926ffb5067b5af3604ab403cfa3dc786149213cc
muhammad faiz <faiz@besquare.com.my>
2022-08-11 15:20:31 +0800

    UI landing page

--- lib/pages/landing_page.dart
+++ lib/pages/landing_page.dart
@@ -8,35 +8,86 @@ class LandingPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Column(
-      children: [
-        ElevatedButton(
-          onPressed: () {
-            Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => const PokeDeckPage(),
-                ));
-          },
-          child: const Text(
-            "POKE DECK",
-            style: TextStyle(fontSize: 20),
+        body: Container(
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage(
+            'lib/background.jpg',
           ),
+          fit: BoxFit.cover,
+          // colorFilter: ColorFilter.mode(Colors.white.withOpacity(0.5), BlendMode.modulate,)
         ),
-        ElevatedButton(
-          onPressed: () {
-            Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => const PokeBattlePage(),
-                ));
-          },
-          child: const Text(
-            "POKE BATTLE",
-            style: TextStyle(fontSize: 20),
-          ),
+      ),
+
+      child: Center(
+
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Image(
+              alignment: Alignment.topCenter,
+              height: 375,
+              width: 375,
+              image: AssetImage('lib/pokemon.png'),
+            ),
+            SizedBox(height: 200),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => const PokeDeckPage(),
+                        ));
+                  },
+                  child: const Text(
+                    "POKE DECK",
+                    style: TextStyle(fontSize: 20,color: Colors.red,fontWeight: FontWeight.bold  ),
+                  ),
+                  style: ButtonStyle(
+                      backgroundColor:MaterialStateProperty.all(Colors.white),
+                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                        RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(18.0),
+                            side: BorderSide(color: Colors.black)
+                        )
+                    ),
+                    
+                    shadowColor: MaterialStateProperty.all(Colors.black)
+                  ),
+                  
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => const PokeBattlePage(),
+                        ));
+                  },
+                  child: const Text(
+                    "POKE BATTLE",
+                    style: TextStyle(fontSize: 20,color: Colors.red,fontWeight: FontWeight.bold),
+                  ),
+                  style: ButtonStyle(
+                    backgroundColor:MaterialStateProperty.all(Colors.white),
+                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                        RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(18.0),
+                            side: BorderSide(color: Colors.black)
+                        )
+                  ),
+
+                ),
+                ),
+              ],
+            ),
+
+          ],
         ),
-      ],
+      ),
     ));
   }
 }
--- lib/pages/poke_battle.dart
+++ lib/pages/poke_battle.dart
@@ -19,7 +19,7 @@ class _PokeBattlePageState extends State<PokeBattlePage> {
   List<Map<String, String>> pokemons = [];
 
   late TextEditingController _pokeController;
-  bool _IsCheckEmpty = false;
+
 
   bool get canSelectMore => selected.length < 2;
 
